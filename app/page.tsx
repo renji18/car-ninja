ae7fdcc21a5ee6fb64cf05b36b64912144ccbdcc
Aadarsh Jha <aadarshjha1401@gmail.com>
2023-07-18 10:31:22 +0530

    catalogue complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import CarCard from "@/components/CarCard"
 import CustomFilter from "@/components/CustomFilter"
 import Hero from "@/components/Hero"
 import SearchBar from "@/components/SearchBar"
+import { fetchCars } from "@/utils"
 import Image from "next/image"
 
-export default function Home() {
+export default async function Home({ searchParams }) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  })
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
+
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -20,6 +32,20 @@ export default function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   )
